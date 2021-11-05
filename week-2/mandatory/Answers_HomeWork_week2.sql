a88ea88313424ef51f29278b422b025c6197884b
Rana_Ahmed <foster3399@gmail.com>
2021-11-06 00:28:43 +0100

    questions 1-12 solved

--- week-2/mandatory/Answers_HomeWork_week2.sql
+++ week-2/mandatory/Answers_HomeWork_week2.sql
@@ -40,10 +40,22 @@ SELECT
 
 --8. Retrieve all orders from customer ID `1`
 SELECT * FROM orders WHERE customer_id = 1;
+
 --9. Retrieve all orders from customer named `Hope Crosby`
+SELECT * FROM 
+	orders o 
+	INNER JOIN customers c ON c.id = o.customer_id
+	WHERE c.name = 'Hope Crosby';
 
 --10. Retrieve all the products in the order `ORD006`. The result should only contain 
 --the columns `product_name`, `unit_price` and `quantity`.  ???
+SELECT  p.product_name,
+        p.unit_price,
+        oItems.quantity
+
+ FROM products p 
+    INNER JOIN order_items oItems ON oItems.product_id = p.id
+    INNER JOIN orders o ON o.id = oItems.order_id WHERE o.order_reference = 'ORD006';
 
 --11. Retrieve all the products with their supplier for all orders of all customers. The result should only contain the columns `name` (from customer), `order_reference` `order_date`, `product_name`, `supplier_name` and `quantity`.
 SELECT c.name,
@@ -58,6 +70,11 @@ SELECT c.name,
     INNER JOIN customers c ON o.id = c.id 
     INNER JOIN suppliers s ON s.id = p.supplier_id
 
-
-
 --12. Retrieve the names of all customers who bought a product from a supplier from China.
+SELECT c.name as customers_bought_from_China 
+    FROM customers c
+        INNER JOIN orders o ON o.customer_id = c.id
+        INNER JOIN order_items oItems ON oItems.order_id = o.id
+        INNER JOIN products p ON p.id = oItems.product_id
+        INNER JOIN suppliers s ON s.id = p.supplier_id
+            WHERE s.country = 'China';
